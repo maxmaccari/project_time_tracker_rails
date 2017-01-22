09e4580569c3a4db1445d1959c1629fd28d667db
Maxsuel Fernandes Maccari <maxmaccari@hotmail.com>
2017-01-22 00:52:03 -0300

    Initial commit

---
+++ spec/rails_helper.rb
@@ -0,0 +1,39 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require 'spec_helper'
+require 'rspec/rails'
+require 'database_cleaner'
+require 'capybara/rspec'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
