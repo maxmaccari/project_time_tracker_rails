af3d006a451930a5b7da5f046b41b1658528ac42
Maxsuel Fernandes Maccari <maxmaccari@hotmail.com>
2017-01-23 14:47:36 -0300

    Add authentication to app

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'bootstrap-sass', '~> 3.3.6'
 
+gem 'devise'
+
 group :development, :test do
   gem 'byebug', platform: :mri
   # Tests
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {
+    new: 'novo', edit: 'editar',
+    sign_in: 'login', sign_out: 'logout', password: 'senha'
+  }
+
+
   root 'projects#index'
   resources :projects, path: ''
 end
