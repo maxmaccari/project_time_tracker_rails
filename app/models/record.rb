53db583256e15829dd588b9dcc96bba5c3e1a9ba
Maxsuel Fernandes Maccari <maxmaccari@hotmail.com>
2017-01-23 20:20:54 -0300

    Internationalize types

--- app/models/record.rb
+++ app/models/record.rb
@@ -4,4 +4,16 @@ class Record < ApplicationRecord
 
   # Associations
   belongs_to :project
+
+  def self.types
+    %w(TimeRecord AmountRecord)
+  end
+
+  def self.human_type_value(type)
+    I18n.t("activerecord.attributes.record.type_values.#{type}")
+  end
+
+  def human_type_value
+    Record.human_type_value(type)
+  end
 end
