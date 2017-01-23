59698390f4e493ce95e26922b31bef4724bde4cc
Maxsuel Fernandes Maccari <maxmaccari@hotmail.com>
2017-01-23 15:17:47 -0300

    Fix routes and forces ssl on production

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root 'projects#index'
+
   devise_for :users, path: '', path_names: {
     new: 'novo', edit: 'editar',
     sign_in: 'login', sign_out: 'logout', password: 'senha'
   }
 
-
-  root 'projects#index'
-  resources :projects, path: ''
+  resources :projects, path: 'projetos', path_names: {  new: 'novo', edit: 'edicao'}
 end
