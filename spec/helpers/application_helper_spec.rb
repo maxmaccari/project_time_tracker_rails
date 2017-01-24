0cd6ec5cd5d57ce138451be6d857ac8780622f02
Maxsuel Fernandes Maccari <maxmaccari@hotmail.com>
2017-01-23 22:59:02 -0300

    Finish application helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,10 +1,18 @@
-require "spec_helper"
+require "rails_helper"
 
-describe ApplicationHelper do
+describe ApplicationHelper, type: :helper do
   describe "#flash_messages" do
     it "include flash messages" do
-      byebug
-      helper.flash_messages.should include('')
+      message = "My Message"
+      flash[:notice] = message
+      helper.flash_messages.should include(message)
+    end
+  end
+
+  describe "#show_layout_flash?" do
+    it "show_layout_flash? returns the value assigned by page_title" do
+      helper.flash_messages(layout_flash: false)
+      helper.show_layout_flash?.should be_falsey
     end
   end
 end
